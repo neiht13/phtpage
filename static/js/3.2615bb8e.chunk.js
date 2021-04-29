255c111d7b66dd92a67206da33fc083cb6c9e403
phan thien <thienphan9612@gmail.com>
2021-04-29 13:41:38 +0700

    Updates

---
+++ static/js/3.2615bb8e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[3],{114:function(t,i,s){"use strict";s.r(i);var c=s(1);i.default=function(t){var i=t.src,s=t.width,a=t.height;return Object(c.jsx)("img",{src:"/img/svg/".concat(i),alt:i,with:s,height:a})}}}]);
+//# sourceMappingURL=3.2615bb8e.chunk.js.map
